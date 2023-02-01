9f2b0774b2fc538d9e4f0a4e01f4b0c1e0cbcc72
Stanislav04 <slavivanov2004@gmail.com>
2023-02-01 18:16:41 +0200

    Load player from ldtk file

--- src/player.rs
+++ src/player.rs
@@ -1,45 +1,60 @@
 use crate::{map::Ground, GameState};
 use bevy::prelude::*;
+use bevy_ecs_ldtk::prelude::*;
 use bevy_rapier2d::prelude::*;
 
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(player_setup_system).add_system_set(
+        app.add_system_set(
             SystemSet::on_update(GameState::MapExploring).with_system(player_movement_system),
-        );
+        )
+        .register_ldtk_entity::<PlayerBundle>("Player");
     }
 }
 
-#[derive(Component)]
-pub struct Player;
-
+const PLAYER_WIDTH: f32 = 60.0;
+const PLAYER_HEIGHT: f32 = 110.0;
 const JUMP_POWER: f32 = 250.0;
 const RUN_POWER: f32 = 100.0;
 
-fn player_setup_system(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands
-        .spawn_bundle(SpriteBundle {
-            texture: asset_server.load("player/player_idle.png"),
-            transform: Transform {
-                translation: Vec3::new(0.0, 0.0, 10.0),
-                scale: Vec3::new(0.5, 0.5, 1.0),
-                ..Default::default()
-            },
-            ..Default::default()
-        })
-        .insert(Player)
-        .insert(Velocity::default())
-        .insert(RigidBody::Dynamic)
-        .insert(Collider::cuboid(30.0, 55.0))
-        .insert(LockedAxes::ROTATION_LOCKED);
+#[derive(Default, Component)]
+pub struct Player;
+
+#[derive(Default, Bundle, LdtkEntity)]
+struct PlayerBundle {
+    #[sprite_sheet_bundle]
+    #[bundle]
+    sprite_sheet_bundle: SpriteSheetBundle,
+    #[from_entity_instance]
+    #[bundle]
+    collider_bundle: ColliderBundle,
+    velocity: Velocity,
+    player: Player,
+}
+
+#[derive(Default, Bundle)]
+struct ColliderBundle {
+    collider: Collider,
+    rigid_body: RigidBody,
+    locked_axes: LockedAxes,
+}
+
+impl From<EntityInstance> for ColliderBundle {
+    fn from(_: EntityInstance) -> Self {
+        Self {
+            collider: Collider::cuboid(PLAYER_WIDTH / 2.0, PLAYER_HEIGHT / 2.0),
+            rigid_body: RigidBody::Dynamic,
+            locked_axes: LockedAxes::ROTATION_LOCKED,
+        }
+    }
 }
 
 fn player_movement_system(
     keyboard_input: Res<Input<KeyCode>>,
     rapier_context: Res<RapierContext>,
-    mut player_info: Query<(Entity, &mut Velocity, &mut Sprite), With<Player>>,
+    mut player_info: Query<(Entity, &mut Velocity, &mut TextureAtlasSprite), With<Player>>,
     tile_info: Query<Entity, With<Ground>>,
 ) {
     let (player, mut velocity, mut sprite) = player_info.single_mut();
@@ -61,7 +76,12 @@ fn player_movement_system(
         for tile in tile_info.iter() {
             if let Some(contact_pair) = rapier_context.contact_pair(player, tile) {
                 for manifold in contact_pair.manifolds() {
-                    if manifold.normal().y == -1.0 {
+                    let first_entity = manifold
+                        .rigid_body1()
+                        .expect("An entity is expected when collision is detected!");
+                    if (first_entity == player && manifold.normal().y == -1.0)
+                        || manifold.normal().y == 1.0
+                    {
                         velocity.linvel.y = JUMP_POWER;
                         return;
                     }
